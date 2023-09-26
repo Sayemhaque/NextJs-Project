8bebed8b04a3c3c87d24615bc68ef61721c37934
Mohammad sayem Haque <85791327+Sayemhaque@users.noreply.github.com>
2023-09-26 12:14:51 +0600

    different page name added

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div>
-           {Math.random() * 123}
-        </div>
+        <div className='flex justify-center flex-col gap-8 items-center h-screen'>
+        <h1 className='text-4xl font-bold'>Our Services</h1>
+    </div>
     );
 };
 
